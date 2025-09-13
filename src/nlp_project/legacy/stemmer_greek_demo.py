5e6e78183ea5afeb34967b2c5242b8656573b99d
Alexandra Kosteletou <kosteletou.alex@gmail.com>
2025-09-13 17:20:48 +0300

    Legacy: skip Greek stemmer demo if 'greek_stemmer' is not installed

--- src/nlp_project/legacy/stemmer_greek_demo.py
+++ src/nlp_project/legacy/stemmer_greek_demo.py
@@ -1,3 +1,9 @@
+try:
+    import greek_stemmer  # ή: from greek_stemmer import stemmer as greek_stemmer
+except Exception as e:
+    print("Skipping Greek stemmer demo: package 'greek_stemmer' not installed:", e)
+    raise SystemExit(0)
+    
 import greek_stemmer
 from greek_stemmer import stemmer
 input= stemmer.stem_word('εργαζόμενος')
