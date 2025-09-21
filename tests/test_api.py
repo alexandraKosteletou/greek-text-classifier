f8ccea1309b04972a9b177571cb2188f7aa92ff4
Alexandra Kosteletou <kosteletou.alex@gmail.com>
2025-09-21 19:12:54 +0300

    fix test script

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,23 @@
 import os
 import pandas as pd
 from fastapi.testclient import TestClient
-from nlp_project.model import TrainConfig, train_and_eval
-from nlp_project.api import app
+# Remove this line: from nlp_project.api import app
+from nlp_project.model import train_and_eval, TrainConfig
 
+def test_api_predict_smoke(tmp_path):
+    # Create model first
+    df = pd.DataFrame({...})
+    mf = tmp_path/'model.joblib'
+    *, * = train_and_eval(df, TrainConfig(ngram_range=(1,2), max_features=1000), model_path=str(mf))
+    
+    # Set env var BEFORE importing app
+    os.environ['MODEL_PATH'] = str(mf)
+    
+    # Import app AFTER env var is set
+    from nlp_project.api import app
+    client = TestClient(app)
+    
+    # Rest of test...
 def test_api_predict_smoke(tmp_path):
     df = pd.DataFrame({
     "text": ["καλό", "κακό", "ουδέτερο", "πολύ καλό", "πολύ κακό", "μάλλον ουδέτερο"],
